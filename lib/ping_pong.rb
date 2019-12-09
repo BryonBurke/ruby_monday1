75302b695a9d120c87f2a635deecf9920f029ec3
Student <example@example.com>
2019-12-09 10:52:55 -0800

    complete program funcitonality

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,27 +1,29 @@
 #!/usr/bin/ruby
-def ping_pong(user_number)
-  number = 0
-  myarray = []
-  user_number.times() do
-    number = number + 1
-    myarray.push(number)
-  end
-  myarray
-end
+# def ping_pong(user_number) #1
+#   number = 0
+#   myarray = []
+#   user_number.times() do #2
+#     number = number + 1
+#     myarray.push(number)
+#   end #2
+#   myarray
+# end #1
 
-def ping_pong(user_number)
+
+def ping_pong(user_number) #1
   number = 0
   myarray = []
-  user_number.times() do
+  user_number.times() do #2
     number = number + 1
-    if number % 3 === 0
-      myarray.push("ding")
-    else if number % 5 === 0
+    if number % 15 === 0
+      myarray.push("ding-dong")
+    elsif number % 5 === 0
       myarray.push("dong")
+    elsif number % 3 === 0
+      myarray.push("ding")
     else
       myarray.push(number)
     end
   end
-  end
   myarray
-end
+end #1
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -7,14 +7,20 @@ require('ping_pong')
 #   end
 # end
 
-# describe('#ping_pong1') do
+# describe('#ping_pong') do
 #   it("replaces instances of numbers divisible by 3, with predefined words") do
 #     expect(ping_pong(3)).to(eq([1,2,"ding"]))
 #   end
 # end
 
-describe('#ping_pong1') do
-  it("replaces instances of numbers divisible by 3 with 'ding' and words divisible by 5 with 'dong'") do
-    expect(ping_pong(6)).to(eq([1,2,"ding",4,"dong","ding"]))
+# describe('#ping_pong') do
+#   it("replaces instances of numbers divisible by 3 with 'ding' and words divisible by 5 with 'dong'") do
+#     expect(ping_pong(6)).to(eq([1,2,"ding",4,"dong","ding"]))
+#   end
+# end
+
+describe('#ping_pong') do
+  it("replaces instances of numbers divisible by 3 with 'ding' and words divisible by 5 with 'dong'. Numbers divisible by 15 are replaced with 'ding-dong'") do
+    expect(ping_pong(16)).to(eq([1,2,"ding",4,"dong","ding",7,8,"ding","dong",11,"ding",13,14,"ding-dong",16]))
   end
 end
